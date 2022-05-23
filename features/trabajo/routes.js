6af4cc7e2ebf7bea87b6d92a35983d517e3d3be1
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-05-23 10:14:36 +0200

    -

---
+++ features/trabajo/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router  = express.Router()
+
+
+// .use <- PARA TODO LO QUE NO SE HAYA MANEJADO
+
+// .get <- Para peticiones GET <-- Recoger Informacion <- MOSTRAR
+// .post <- Para peticiones POST <-- Guardar Informacion <- CREAR
+// .put <- Para peticiones PUT <-- Actualizar Informacion <- MODIFICAR
+// .delete <- Para peticiones DELETE <-- Eliminar Informacion <- ELIMINAR
+
+
+module.exports = router;
--- routes.js
+++ routes.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const router  = express.Router()
 
-const fNoches = require('./razas.functions')
+const rHabilidades = require('./features/habilidades/routes')
+const rRaza        = require('./features/raza/routes')
+const rTrabajo     = require('./features/trabajo/routes')
+const rRelaciones  = require('./features/relaciones/routes')
+const rLugares     = require('./features/lugares/routes')
 
-router.use('/crear_noches', fNoches.get_noches)
+const rPersona   = require('./features/persona/routes')
+const rAnecdotas = require('./features/anecdotas/routes')
+const rPersonaje = require('./features/personaje/routes')
+const rCapitulos = require('./features/capitulos/routes')
+
+router.use('/habilidades', rHabilidades)
+router.use('/raza', rHabilidades)
+router.use('/trabajo', rHabilidades)
+router.use('/relaciones', rHabilidades)
+router.use('/lugares', rHabilidades)
+
+router.use('/persona', rHabilidades)
+router.use('/anecdotas', rHabilidades)
+router.use('/personaje', rHabilidades)
+router.use('/capitulos', rHabilidades)
+
+
+// .use <- PARA TODO LO QUE NO SE HAYA MANEJADO
+
+// .get <- Para peticiones GET <-- Recoger Informacion <- MOSTRAR
+// .post <- Para peticiones POST <-- Guardar Informacion <- CREAR
+// .put <- Para peticiones PUT <-- Actualizar Informacion <- MODIFICAR
+// .delete <- Para peticiones DELETE <-- Eliminar Informacion <- ELIMINAR
+
 
 module.exports = router;
