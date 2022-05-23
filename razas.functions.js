d1155b32ca2e84ed472a31e2c7b39019945370d7
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-05-23 10:14:31 +0200

    -

--- razas.functions.js
+++ razas.functions.js
@@ -6,28 +6,7 @@ const get_noches = async (req, res) => {
     let orderBy = {}
     orderBy[order] = 1
 
-    // ARRAY
-    const noches = [
-        {
-            nombre: 'Noche 1',
-            fecha: '2020-01-01',
-            hora: '00:00',
-            duracion: '00:00',
-            descripcion: 'Descripcion de la noche 1',
-            razas: [
-                {
-                    nombre: 'Raza 1',
-                    descripcion: 'Descripcion de la raza 1',
-                    personajes: [
-                        {
-                            nombre: 'Personaje 1',
-                            apodo: 'Personaje 1'
-                        }
-                    ]
-                }
-            ]
-        },
-    ]
+
 
     const data = {
         noches:noches
